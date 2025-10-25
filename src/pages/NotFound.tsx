c59c70ec2dc032ccfbdb0a640d79e084331f90c8
dungcam16 <127020149+dungcam16@users.noreply.github.com>
2025-10-25 13:12:34 +0700

    AI: Fix NotFound.tsx to use export default function syntax

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,7 +1,8 @@
+"use client";
 import { useLocation } from "react-router-dom";
-import { useEffect } from "react";
+import React, { useEffect } from "react";
 
-const NotFound = () => {
+export default function NotFound() {
   const location = useLocation();
 
   useEffect(() => {
@@ -19,6 +20,4 @@ const NotFound = () => {
       </div>
     </div>
   );
-};
-
-export default NotFound;
+}
